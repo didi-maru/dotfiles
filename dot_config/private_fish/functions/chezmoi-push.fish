1cedd1ac526bbfb044997249e3c9a3d5aeecbc00
Adrien Martinez <64746464+didi-maru@users.noreply.github.com>
2021-12-11 18:44:12 +0100

    commit from didi@fedora  at sam. 11 déc. 2021 18:44:12 CET

--- dot_config/private_fish/functions/chezmoi-push.fish
+++ dot_config/private_fish/functions/chezmoi-push.fish
@@ -1,8 +1,14 @@
 function chezmoi-push --description 'push all change to chezmoi dotfile git'
-set --local current_pwd (pwd)
-cd ~/.local/share/chezmoi
-git add .
-git commit -m "commit from $USER@$hostname  at "(date)
-git push
-cd "$current_pwd"
+    set --local current_pwd (pwd)
+    cd ~/.local/share/chezmoi
+
+    set_color blue; echo "Pulling dotfile repository ..."; set_color normal
+    git pull
+
+    set_color blue; echo "Commit changes and pushing ..."; set_color normal
+    git add .
+    git commit -m "commit from $USER@$hostname  at "(date)
+    git push
+    
+    cd "$current_pwd"
 end
