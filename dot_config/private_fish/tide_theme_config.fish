ecd1ee32c9fab58098b6a97541c2305750ba7da5
Adrien Martinez <64746464+didi-maru@users.noreply.github.com>
2022-01-22 20:04:58 +0100

    commit from didi@fedora  at sam. 22 janv. 2022 20:04:58 CET

--- dot_config/private_fish/tide_theme_config.fish
+++ dot_config/private_fish/tide_theme_config.fish
@@ -1,6 +1,6 @@
 tide_character_bg_color normal
-tide_character_color 5FD700
-tide_character_color_failure FF0000
+tide_character_color ABE9B3
+tide_character_color_failure F28FAD
 tide_character_icon ❯
 tide_character_vi_icon_default ❮
 tide_character_vi_icon_replace ▶
@@ -8,32 +8,32 @@ tide_character_vi_icon_visual V
 tide_chruby_bg_color B31209
 tide_chruby_color 000000
 tide_chruby_icon 
-tide_cmd_duration_bg_color C4A000
-tide_cmd_duration_color 000000
+tide_cmd_duration_bg_color FAE3B0
+tide_cmd_duration_color
 tide_cmd_duration_decimals 0
 tide_cmd_duration_icon
 tide_cmd_duration_threshold 3000
 tide_context_always_display false
-tide_context_bg_color 444444
-tide_context_color_default D7AF87
-tide_context_color_root D7AF00
-tide_context_color_ssh D7AF87
-tide_git_bg_color 4E9A06
-tide_git_bg_color_unstable C4A000
-tide_git_bg_color_urgent CC0000
-tide_git_color_branch 000000
-tide_git_color_conflicted 000000
-tide_git_color_dirty 000000
-tide_git_color_operation 000000
-tide_git_color_staged 000000
-tide_git_color_stash 000000
-tide_git_color_untracked 000000
-tide_git_color_upstream 000000
+tide_context_bg_color 575268
+tide_context_color_default FAE3B0
+tide_context_color_root F8BD96
+tide_context_color_ssh FAE3B0
+tide_git_bg_color ABE9B3
+tide_git_bg_color_unstable FAE3B0
+tide_git_bg_color_urgent F28FAD
+tide_git_color_branch 131020
+tide_git_color_conflicted 131020
+tide_git_color_dirty 131020
+tide_git_color_operation 131020
+tide_git_color_staged 131020
+tide_git_color_stash 131020
+tide_git_color_untracked 131020
+tide_git_color_upstream 131020
 tide_git_icon
 tide_go_bg_color 00ACD7
 tide_go_color 000000
 tide_go_icon 
-tide_jobs_bg_color 444444
+tide_jobs_bg_color 575268
 tide_jobs_color 4E9A06
 tide_jobs_icon 
 tide_kubectl_bg_color 326CE5
@@ -48,28 +48,28 @@ tide_left_prompt_suffix ▓▒░
 tide_node_bg_color 44883E
 tide_node_color 000000
 tide_node_icon ⬢
-tide_os_bg_color CED7CF
-tide_os_color 080808
+tide_os_bg_color B5E8E0
+tide_os_color 302D41
 tide_os_icon 
 tide_php_bg_color 617CBE
 tide_php_color 000000
 tide_php_icon 
 tide_prompt_add_newline_before true
-tide_prompt_color_frame_and_connection 6C6C6C
-tide_prompt_color_separator_same_color 949494
-tide_prompt_icon_connection ''  ''
+tide_prompt_color_frame_and_connection 6E6C7E
+tide_prompt_color_separator_same_color 988BA2
+tide_prompt_icon_connection ' '
 tide_prompt_min_cols 26
 tide_prompt_pad_items true
-tide_pwd_bg_color 3465A4
-tide_pwd_color_anchors E4E4E4
-tide_pwd_color_dirs E4E4E4
-tide_pwd_color_truncated_dirs BCBCBC
+tide_pwd_bg_color 89DCEB
+tide_pwd_color_anchors 131020
+tide_pwd_color_dirs 131020
+tide_pwd_color_truncated_dirs 302D41
 tide_pwd_icon
 tide_pwd_icon_home
 tide_pwd_icon_unwritable 
 tide_pwd_markers '.bzr'  '.citc'  '.git'  '.hg'  '.node-version'  '.python-version'  '.ruby-version'  '.shorten_folder_marker'  '.svn'  '.terraform'  'Cargo.toml'  'composer.json'  'CVS'  'go.mod'  'package.json'
 tide_right_prompt_frame_enabled false
-tide_right_prompt_items 'status'  'cmd_duration'  'context'  'jobs'  'virtual_env'  'rustc'  'vi_mode'
+tide_right_prompt_items 'status'  'cmd_duration'  'context'  'jobs'  'node'  'virtual_env'  'vi_mode'
 tide_right_prompt_prefix ░▒▓
 tide_right_prompt_separator_diff_color 
 tide_right_prompt_separator_same_color 
@@ -81,14 +81,14 @@ tide_shlvl_bg_color 808000
 tide_shlvl_color 000000
 tide_shlvl_icon 
 tide_shlvl_threshold 1
-tide_status_bg_color 2E3436
-tide_status_bg_color_failure CC0000
-tide_status_color 4E9A06
-tide_status_color_failure FFFF00
+tide_status_bg_color 575268
+tide_status_bg_color_failure F28FAD
+tide_status_color ABE9B3
+tide_status_color_failure FAE3B0
 tide_status_icon ✔
 tide_status_icon_failure ✘
-tide_time_bg_color D3D7CF
-tide_time_color 000000
+tide_time_bg_color C3BAC6
+tide_time_color 131020
 tide_time_format
 tide_vi_mode_bg_color_default 008000
 tide_vi_mode_bg_color_replace 808000
